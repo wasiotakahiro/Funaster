eccca6bbcac1ef58038078581c2ed004360b52c7
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-08-21 20:22:22 +0900

    uploder retouch

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  storage :file
+
+  process :resize_to_limit => [50, 50] 
 
   # Choose what kind of storage to use for this uploader:
   storage :file
